fae8520f659d3aa6e75d9a5f1fde92102cbdb00f
Jay Zehngebot <jay.zehngebot@gmail.com>
2013-09-17 12:45:43 -0400

    Initial Commit

---
+++ bicycle/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  bicycle
+//
+//  Created by Jay Zehngebot on 9/17/13.
+//  Copyright (c) 2013 Jay Zehngebot. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
