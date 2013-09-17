adfaf980df6aaf98887017ea7e958278df1faf4b
Jay Zehngebot <jay.zehngebot@gmail.com>
2013-09-17 13:57:24 -0400

    first

--- bicycle/main.cpp
+++ bicycle/main.cpp
@@ -1,18 +1,56 @@
 //
 //  main.cpp
-//  bicycle
-//
-//  Created by Jay Zehngebot on 9/17/13.
-//  Copyright (c) 2013 Jay Zehngebot. All rights reserved.
+//  bicycleFrame
 //
 
 #include <iostream>
+#include <math.h>
 
+using namespace std;
+
+// bicycle class
+
+struct BicycleFrame
+{
+    float bbstackHeight;
+    float bbDrop;
+    float chainStayLength;
+    float drop;
+    int forkRake;
+    
+    float frontForkStackHeight;
+    float frontCenterLength;
+    float headTubeLength;
+    float headtubeAngle;
+    float headtubeHeight;
+    
+    float reach;
+    float rearCenterLength;
+    float saddleToPedalDist;
+    float seatTubeAngle;
+    float seatingAngle;
+    
+    float seatTubeCenterToTop;
+    float seatTubeHeight;
+    float topTubeEffectiveLength;
+    float topTubeLength;
+    float wheelbaseDist;
+    
+    float steelTubeRadius;
+    std::string	modelName;
+ 
+    //constructor
+    BicycleFrame(){
+        cout << "Built a BikeFrame" << endl;
+    }
+};
+
+
+// main funct
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    BicycleFrame bikeFrame = BicycleFrame();
+    cout << "Total Cost in Memory " << sizeof( bikeFrame ) << " byte(s)." << endl;
     return 0;
 }
 
